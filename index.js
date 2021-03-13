6ebee2f70568ea0f6836a8974e34c86c53d02033
syd333 <sydmiranda@icloud.com>
2021-03-12 17:10:08 -0800

    commit tree 79d89d2b9b15433c269d554f1c2b8a100e267cbd

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = drivers => {
+  return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = drivers => {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = int => {
+    return (val) => int * val
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (arr, func) => {
+    return func(arr);
+}
